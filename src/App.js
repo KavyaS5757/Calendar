f059180718ce2e3ed2c7cb06194b1f64179ab550
Kavya Subramanian <kavyasmallika@gmail.com>
2024-01-21 15:01:10 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Calendar from "./Calendar";
 
-function App() {
+const Events = ({ events, onAddEvent }) => {
+  const [newEvent, setNewEvent] = useState("");
+
+  const handleAddEvent = () => {
+    if (newEvent.trim() !== "") {
+      onAddEvent(newEvent);
+      setNewEvent("");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="events">
+      <h3>Events</h3>
+      <ul>
+        {events.map((event, index) => (
+          <li key={index}>{event}</li>
+        ))}
+      </ul>
+      <div>
+        <input
+          type="text"
+          value={newEvent}
+          onChange={(e) => setNewEvent(e.target.value)}
+        />
+        <button onClick={handleAddEvent}>Add Event</button>
+      </div>
     </div>
   );
-}
+};
+
+
+
+const App = () => {
+  const [events, setEvents] = useState([]);
+  const [calendarOptions, setCalendarOptions] = useState({});
+
+  const handleDropdownChange = (options) => {
+    setCalendarOptions((prevOptions) => ({ ...prevOptions, ...options }));
+  };
+
+  const handleAddEvent = (newEvent) => {
+    setEvents((prevEvents) => [...prevEvents, newEvent]);
+  };
+
+
+  return (
+    <div className="container">
+      <div className="left-side">
+        <Events events={events} onAddEvent={handleAddEvent} />
+        
+      </div>
+      <div className="right-side">
+        <div className="main">
+          <Calendar {...calendarOptions} events={events} />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default App;
